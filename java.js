1cc438e1bcc2ed119c44ed319b7f21a3ff0579aa
Meera Al Kuwaiti <meera@Meeras-MacBook-Pro-3.local>
2019-08-30 23:27:53 +0400

    login

--- java.js
+++ java.js
@@ -1,17 +1,12 @@
+var productsData;
 fetch("./products.json")
   .then(response => response.json())
-  .then(data => displayProducts(data.products));
+  .then(data => {
+    productsData = data;
+    displayProducts(data);
+  });
 
-function createProducts(products) {
-  for (let value of products) {
-    for (let index in value) {
-      var div = document.createElement("div");
-      div.innerHTML = value[index];
-      document.getElementById("container").appendChild(div);
-    }
-  }
-}
-
+// display products
 function displayProducts(obj) {
   for (let value of obj) {
     var div = document.createElement("div");
@@ -36,32 +31,71 @@ function displayProducts(obj) {
   }
 }
 
-function createTable(records) {
-  var table = document.createElement("table");
-  table.setAttribute("border", 1);
-  table.appendChild(createHeading(records[0]));
-  for (let record of records) {
-    table.appendChild(createRow(record));
-  }
-  document.getElementById("container").appendChild(table);
+function fillterdProducts() {
+  var search_input = document.getElementById("searchBarInput").value;
+  document.getElementById("products").innerHTML = "";
+  displayProducts(
+    productsData.filter(value => {
+      var lowerCaseProduct = value.name.toLowerCase();
+      return lowerCaseProduct.includes(search_input);
+    })
+  );
 }
 
-function createHeading(record) {
-  var row = document.createElement("tr");
-  for (let prop in record) {
-    var heading = document.createElement("th");
-    heading.innerHTML = prop.toUpperCase();
-    row.appendChild(heading);
+function createProducts(products) {
+  for (let value of products) {
+    for (let index in value) {
+      var div = document.createElement("div");
+      div.innerHTML = value[index];
+      document.getElementById("container").appendChild(div);
+    }
   }
-  return row;
 }
 
-function createRow(record) {
-  var row = document.createElement("tr");
-  for (let prop in record) {
-    var column = document.createElement("td");
-    column.innerHTML = record[prop];
-    row.appendChild(column);
+function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username == null || username == "") {
+    alert("Please enter the username.");
+    return false;
   }
-  return row;
+  if (password == null || password == "") {
+    alert("Please enter the password.");
+    return false;
+  }
+  if ((password == "123") & (username == "meera")) {
+    return alert("Login successful");
+  } else {
+    return alert("Please try again");
+  }
+
+  function createTable(records) {
+    var table = document.createElement("table");
+    table.setAttribute("border", 1);
+    table.appendChild(createHeading(records[0]));
+    for (let record of records) {
+      table.appendChild(createRow(record));
+    }
+    document.getElementById("container").appendChild(table);
+  }
+
+  function createHeading(record) {
+    var row = document.createElement("tr");
+    for (let prop in record) {
+      var heading = document.createElement("th");
+      heading.innerHTML = prop.toUpperCase();
+      row.appendChild(heading);
+    }
+    return row;
+  }
+
+  function createRow(record) {
+    var row = document.createElement("tr");
+    for (let prop in record) {
+      var column = document.createElement("td");
+      column.innerHTML = record[prop];
+      row.appendChild(column);
+    }
+    return row;
+  }
 }
